e018dad163ad34b861a7d78d79849f91135d6b4d
ozihler <ozihler@gmail.com>
2020-06-28 10:21:05 +0200

    use elvis operator

--- src/Results.js
+++ src/Results.js
@@ -3,29 +3,21 @@ import Pet from './Pet';
 // One way data flow parent => child. No data flow child => parent!
 // It's intentionally difficult to go back up! You should usually NOT go up again!
 const Results = ({pets}) => {
-    if (!pets.length) {
-        return (
-            <div className="search">
-                {<h1>No Pets Found</h1>}
-            </div>
-        );
-    } else {
-        return (
-            <div className="search">
-                {pets.map(pet => (
-                    <Pet
-                        animal={pet.type}
-                        key={pet.id}
-                        name={pet.name}
-                        breed={pet.breeds.primary}
-                        media={pet.photos}
-                        location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
-                        id={pet.id}
-                    />
-                ))}
-            </div>
-        );
-    }
+    return (
+        <div className="search">
+            {!pets.length ? <h1>No Pets Found</h1> : (pets.map(pet => (
+                <Pet
+                    animal={pet.type}
+                    key={pet.id}
+                    name={pet.name}
+                    breed={pet.breeds.primary}
+                    media={pet.photos}
+                    location={`${pet.contact.address.city}, ${pet.contact.address.state}`}
+                    id={pet.id}
+                />
+            )))}
+        </div>
+    );
 }
 
 export default Results;
